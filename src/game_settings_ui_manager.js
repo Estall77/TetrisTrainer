b4fdd9db6e0edeefa1fa73bfb40c77394102a128
evesy <64846642+Estall77@users.noreply.github.com>
2025-01-18 15:42:53 +0000

    Update game_settings_ui_manager.js

--- src/game_settings_ui_manager.js
+++ src/game_settings_ui_manager.js
@@ -31,6 +31,7 @@ const DAS_SPEED_LIST = [
   DASSpeed.MEDIUM,
   DASSpeed.FAST,
   DASSpeed.FASTDAS,
+  DASSpeed.SUPERFAST
 ];
 
 /* List of DAS charging behaviors in order that they're listed in the UI dropdown.
